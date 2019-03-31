39b8d708b83d87b1256babd59a21dd60be59c588
hyunwook <c004245@naver.com>
2019-03-31 15:52:46 +0900

    Custom Calendar View

---
+++ app/src/main/java/com/playgilround/schedule/client/calendarschedule/view/CalendarView.java
@@ -0,0 +1,18 @@
+package com.playgilround.schedule.client.calendarschedule.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import java.util.jar.Attributes;
+
+public class CalendarView extends LinearLayout {
+
+    public CalendarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
