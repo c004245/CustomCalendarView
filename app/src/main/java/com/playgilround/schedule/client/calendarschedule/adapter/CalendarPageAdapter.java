826508be1f780fb5a9fa48f380d06f9c3e492a23
hyunwookcho <c004245@naver.com>
2019-04-02 18:14:03 +0900

    PagerAdapter 구조생성

--- app/src/main/java/com/playgilround/schedule/client/calendarschedule/adapter/CalendarPageAdapter.java
+++ app/src/main/java/com/playgilround/schedule/client/calendarschedule/adapter/CalendarPageAdapter.java
@@ -2,6 +2,11 @@ package com.playgilround.schedule.client.calendarschedule.adapter;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+import com.playgilround.schedule.client.calendarschedule.util.CalendarProperties;
+
+import static com.playgilround.schedule.client.calendarschedule.util.CalendarProperties.CALENDAR_SIZE;
 
 /**
  * 달력 페이지 로드 Adapter
@@ -9,5 +14,22 @@ import android.support.v4.view.PagerAdapter;
 public class CalendarPageAdapter extends PagerAdapter {
 
     private Context mContext;
+
+    private CalendarProperties mCalendarProperties;
+
+    public CalendarPageAdapter(Context context, CalendarProperties calendarProperties) {
+        mContext = context;
+        mCalendarProperties = calendarProperties;
+    }
+
+    @Override
+    public int getCount() {
+        return CALENDAR_SIZE;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
 
 }
