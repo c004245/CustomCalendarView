b1d473c591e2fce0cf60a72f5595ed8452546135
hyunwook <c004245@naver.com>
2019-04-21 15:32:48 +0900

    Add ButterKnife library, Schedule Adapter class

---
+++ app/src/main/java/com/playgilround/schedule/client/calendarschedule/adapter/ScheduleAdapter.java
@@ -0,0 +1,51 @@
+package com.playgilround.schedule.client.calendarschedule.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.playgilround.schedule.client.calendarschedule.R;
+
+import androidx.annotation.NonNull;
+import butterknife.ButterKnife;
+
+/**
+ * RecyclerView 에 표시 될 스케줄 목록 Adapter
+ */
+public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHolder> {
+
+    public ScheduleAdapter() {
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.item_schedule, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bindView(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5; //Test
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        void bindView(int position) {
+
+        }
+    }
+}
